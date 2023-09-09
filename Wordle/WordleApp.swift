ff0c8ae52a25bc54334da3821f6f1e5104188198
Holger Becker <h.becker@chhb.de>
2023-09-09 17:13:31 +0200

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Holger Becker on 09.09.23.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
