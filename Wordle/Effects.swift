f932f4df97079b7b787b9595a32a6f339e1c0c7f
swiftyHolli <h.becker@chhb.de>
2023-09-26 21:53:26 +0200

    Add Effects

---
+++ Wordle/Effects.swift
@@ -0,0 +1,24 @@
+//
+//  Effects.swift
+//  Wordle
+//
+//  Created by Holger Becker on 26.09.23.
+//
+
+import SwiftUI
+
+struct ShakeEffect: GeometryEffect {
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        return ProjectionTransform(CGAffineTransform(translationX: -30 * sin(position * 2 * .pi), y: 0))
+    }
+    
+    init(shakes: Int) {
+        position = CGFloat(shakes)
+    }
+    
+    var position: CGFloat
+    var animatableData: CGFloat {
+        get { position }
+        set { position = newValue }
+    }
+}
