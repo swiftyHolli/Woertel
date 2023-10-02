ec2c8e9af1908b1a4119b5fdb3a86c340668486f
swiftyHolli <h.becker@chhb.de>
2023-10-02 22:04:32 +0200

    info view geometry constants

---
+++ Wordle/InfoPage2.swift
@@ -0,0 +1,18 @@
+//
+//  InfoPage2.swift
+//  Wordle
+//
+//  Created by Holger Becker on 01.10.23.
+//
+
+import SwiftUI
+
+struct InfoPage2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoPage2()
+}
