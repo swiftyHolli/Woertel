2cf00633fef02ce40ee2a6e7de436dc8330f5e2d
swiftyHolli <h.becker@chhb.de>
2023-10-06 00:26:51 +0200

    new Statistics view

---
+++ Wordle/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  Wordle
+//
+//  Created by Holger Becker on 05.10.23.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
