e64add70d77a3ca840aaad20d6212c6e698d1a58
swiftyHolli <h.becker@chhb.de>
2023-10-12 22:46:29 +0200

    Fast fertig

---
+++ Wordle/FlyingScore.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingScore.swift
+//  Wordle
+//
+//  Created by Holger Becker on 12.10.23.
+//
+
+import SwiftUI
+
+struct FlyingScore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingScore()
+}
